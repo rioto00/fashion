ba084464a7877aeec0a959865495f89d7c07d0a4
rioto00 <rioto.t0817@gmail.com>
2024-04-17 12:32:56 +0000

    public/comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,14 +3,15 @@ class Public::CommentsController < ApplicationController
   
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.build(comment_params)
-    @comment.post = @post
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
   
     if @comment.save
       flash[:success] = "コメントが投稿されました"
       redirect_to post_path(@post)
     else
       flash[:error] = "コメントの投稿に失敗しました"
+      @posts = [@post]
       render 'public/posts/show'
     end
   end
@@ -20,5 +21,4 @@ class Public::CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :other_attribute)
   end
-  
 end
