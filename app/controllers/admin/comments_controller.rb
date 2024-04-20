d9bab2f839671f44a82685341f7c7b0b29d8d165
rioto00 <rioto.t0817@gmail.com>
2024-04-20 09:03:07 +0000

    comments

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,23 @@
+class Admin::CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
+  
+  def index
+    @comments = Comment.all
+  end
+
+  def destroy
+    set_comment
+    @comment.destroy
+    redirect_to admin_comments_path, notice: "投稿を削除しました"
+  end
+  
+  private
+  
+  def set_comment 
+    @comment = Comment.find_by(id: params[:id])
+    unless @comment
+      redirect_to admin_comments_path, alert: "指定されたコメントが見つかりませんでした。"
+    end
+  end
+end
+
